572fc3138a5427ecbd2530e744c42f4d0e8b647f
Miguel Alcântara <miguel.alcantara@innowave.tech>
2019-02-20 15:44:55 +0000

    Initial Commit

---
+++ Minius/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minius
+//
+//  Created by Miguel Alcântara on 20/02/2019.
+//  Copyright © 2019 Miguel Alcântara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
